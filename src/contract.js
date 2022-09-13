decae9931e17e003d6b464812aa7112832c891cb
tmrbv <71847119+tmrbv@users.noreply.github.com>
2022-09-13 14:14:14 +0100

    big commit

--- src/contract.js
+++ src/contract.js
@@ -1,18 +1,57 @@
-import { NearBindgen, NearPromise, near, call, view, bytes } from 'near-sdk-js';
+import { NearBindgen, NearPromise, near, call, view, bytes, assert } from 'near-sdk-js';
 import { PublicKey } from 'near-sdk-js/lib/types'
+
+const GLUE_ID = 'dev-1663003785845-27921924996527'
 
 @NearBindgen({})
 class HelloNear {
 
-  @call({})
-  test_deploy({ }) {
-    let promise = NearPromise.new('abc.dev-1663003785845-27921924996527')
+  @call({payableFunction: true})
+  deploy({ }) {
+    assert(near.attachedDeposit() >= BigInt('20000000000000000000000000'), "not enough deposit")
+    let signer = near.signerAccountId()
+    let subaccount = signer.substring(0, signer.length - 7) + GLUE_ID
+    let promise = NearPromise.new(subaccount)
 
     // possible promise actions, choose and chain what you need:
     promise.createAccount()
-    .transfer(10_000_000_000_000_000_000_000_000n)
-    .addFullAccessKey(new PublicKey(near.signerAccountPk()))
-    .deployContract(includeBytes('../../../../../../Downloads/fungible_token.wasm'))
+      .transfer(20_000_000_000_000_000_000_000_000n)
+      .addFullAccessKey(new PublicKey(near.signerAccountPk()))
+      .deployContract(includeBytes('../../../../../../Downloads/fungible_token.wasm'))
+    return promise
+  }
+
+  @call({})
+  createToken({ tokenName, supply, decimals, canMint, canBurn, symbol }) {
+    let signer = near.signerAccountId()
+    let subaccount = signer.substring(0, signer.length - 7) + GLUE_ID
+    let promise = NearPromise.new(subaccount)
+
+    promise.functionCall('new', JSON.stringify({
+      total_supply: supply,
+      metadata: {
+        spec: "ft-1.0.0",
+        name: tokenName,
+        symbol: symbol,
+        decimals: decimals,
+      },
+      can_mint: canMint,
+      can_burn: canBurn,
+      glue_id: 'dev-1663003785845-27921924996527',
+    }), '0', '5000000000000')
+    return promise
+  }
+
+  @call({})
+  ft_transfer({ receiver_id, amount }) {
+    let signer = near.signerAccountId()
+    let subaccount = signer.substring(0, signer.length - 7) + GLUE_ID
+    let promise = NearPromise.new(subaccount)
+
+    promise.functionCall('ft_transfer', JSON.stringify({
+      receiver_id: receiver_id,
+      amount: amount,
+    }), '0', '5000000000000')
 
     return promise
   }
